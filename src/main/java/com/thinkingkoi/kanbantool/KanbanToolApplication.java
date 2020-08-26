d19295c289eb9b4aea344c350b73b8d92afb4742
thais17 <thai.nguyen1@onemount.com>
2020-08-26 21:46:30 +0700

    First commit

---
+++ src/main/java/com/thinkingkoi/kanbantool/KanbanToolApplication.java
@@ -0,0 +1,13 @@
+package com.thinkingkoi.kanbantool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KanbanToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thinkingkoi/kanbantool/KanbanToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinkingkoi.kanbantool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
